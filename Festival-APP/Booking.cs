efe20ea83968c12ef9fcbf1667379f8a83fa5f38
Serdar Bozkurt <r0960532@uccl.be>
2024-06-23 13:50:55 +0200

    Add project files.

---
+++ Festival-APP/Booking.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festival_APP
+{
+    public class BookingException : Exception
+    {
+        public BookingException(string message) : base(message)
+        {
+        }
+    }
+    internal class Booking
+    {
+        private int start;
+        private int end;
+        private List <string> RiderItem;
+        public int Id { get; }
+        public string GroupName { get; set; }
+        public int StartHour
+        {
+            get
+            {
+                return start;
+            }
+            set
+            {
+                if (value < 0 || value > 23) 
+                {
+                    throw new BookingException("Startuur mag niet lager zijn dan 0 of hoger dan 23");
+                }
+                else 
+                { 
+                    start = value;
+                }
+            }
+        }
+        public int EndHour {
+            get
+            {
+                return end;
+            }
+            set
+            {
+                if(value <= start || value < 0 || value > 23) 
+                {
+                    throw new BookingException("Het einduur moet hoger zijn dan 0 en lager zijn dan 23 en mag niet voor het startuur zijn");
+                } 
+                else 
+                {
+                        end = value;
+                }
+                
+            }    
+        }
+        public int Duration { 
+            get
+            {
+                return (EndHour - StartHour);
+            }
+        }
+        public Booking(int id, string groupName, int startHour, int endHour) 
+        {
+            this.Id = id;
+            this.GroupName = groupName;
+            this.StartHour = startHour;
+            this.EndHour = endHour;
+            this.RiderItem = new List<string>();
+        }
+        public List<string> RiderItems {
+            get
+            {
+                return RiderItem;
+            }
+        }
+        public void addRiderItem(string riderItem) 
+        {
+            RiderItem.Add(riderItem);
+        }
+        public bool conflictsWith(Booking booking) 
+        {
+            if(this.StartHour < booking.EndHour && this.EndHour > booking.StartHour) 
+            {
+                return true;
+            } else 
+            { 
+                return false; 
+            }
+        }
+    }
+}
---
+++ Festival-APP/Program.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.Design;
+
+namespace Festival_APP
+{
+    internal class Program
+    {
+        private static string MENU = 
+@"Beheer festival.
+A> Groep boeken
+B> Groep verwijderen
+C> Boekingen bekijken
+D> Rider toevoegen
+E> Rider bekijken
+Q> Verlaten";
+        static void Main(string[] args)
+        {
+            List<Booking> bookings = new List<Booking>();
+            int bookingCount = 1;
+            int id;
+            Console.WriteLine();
+            while(true) 
+            {
+                Console.WriteLine(MENU);
+                Console.Write("> ");
+                string input = Console.ReadLine().ToUpper();
+                bool conflicts = false, found = false;
+
+                try
+                {
+                    switch (input) 
+                    {
+                    case "A":
+                        Console.Write("Groep: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Uur start: ");
+                        int startHour = int.Parse(Console.ReadLine());
+                        Console.Write("Uur einde: ");
+                        int endHour = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        foreach (Booking book in bookings)
+                        {
+                            if (book.conflictsWith(new Booking(bookingCount, name, startHour, endHour)))
+                            {
+                                conflicts = true;
+                                break;
+                            }
+                        }
+                        if (conflicts)
+                        {
+                            Console.WriteLine("Uren zijn al bezet");
+                        }
+                        else
+                        {
+                            bookings.Add(new Booking(bookingCount++, name, startHour, endHour));
+                        }
+                        break;
+                    case "B":
+                        foreach (Booking book in bookings)
+                        {
+                            Console.WriteLine($"{book.Id} - {book.GroupName} van {book.StartHour} tot {book.EndHour} ({book.Duration} uur)");
+                        }
+                        Console.WriteLine("Verwijder");
+                        Console.Write("> ");
+                        id = int.Parse(Console.ReadLine());
+
+              
+                        foreach (Booking book in bookings)
+                        {
+                            if (book.Id == id)
+                            {
+                                found = true;
+                                bookings.Remove(book);
+                                Console.WriteLine($"Keuze {id}  is verwijderd");
+                                break;
+                            }
+                        }
+                        if (!found){
+                            Console.WriteLine("Uw keuze bestaat niet");
+                        }
+                        break;
+                    case "C":
+                        foreach (Booking book in bookings) 
+                        {
+                            Console.WriteLine($"{book.Id} - {book.GroupName} van {book.StartHour} tot {book.EndHour} ({book.Duration} uur)");
+                        }
+                        break;
+                    case "D":
+                        Console.WriteLine("Kies een groep om rider te bewerken");
+                        foreach (Booking book in bookings)
+                        {
+                            Console.WriteLine($"{book.Id} - {book.GroupName} van {book.StartHour} tot {book.EndHour}  ({book.Duration} uur)");
+                        }
+                        Console.Write("> ");
+                        id = int.Parse(Console.ReadLine());
+
+                        foreach (Booking book in bookings)
+                        {
+                            if (book.Id == id)
+                            {
+                                found = true;
+                                Console.WriteLine("Geef een beschrijving: ");
+                                string itemAdd = Console.ReadLine();
+                                book.addRiderItem(itemAdd);
+                                Console.WriteLine($"\"{itemAdd}\" toegevoegd aan rider");
+                                break;
+                            }
+                        }
+                        if (!found)
+                        {
+                            Console.WriteLine("Uw keuze bestaat niet");
+                        }
+                        break;
+                    case "E":
+                        Console.WriteLine("Kies een groep om rider te bekijken");
+                        foreach (Booking book in bookings)
+                        {
+                            Console.WriteLine($"{book.Id} - {book.GroupName} van {book.StartHour} tot {book.EndHour}  ({book.Duration} uur)");
+                        }
+                        id = int.Parse(Console.ReadLine());
+
+         
+                        foreach (Booking book in bookings)
+                        {
+                            if (book.Id == id)
+                            {
+                                found = true;
+                                foreach (string riderItem in book.RiderItems)
+                                {
+                                    Console.WriteLine(riderItem);
+                                }
+                                break;
+
+                            }
+                    
+                        }
+                        if(!found)
+                        {
+                            Console.WriteLine("Uw keuze bestaat niet");
+                        }
+                        break;
+                    case "Q":
+                        Console.WriteLine("Tot ziens!");
+                        return;
+                    default:
+                        Console.WriteLine("Ongeldige input");
+                        break;
+                    }
+                }
+                catch (BookingException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
